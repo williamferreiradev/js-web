2addea0ba4e4f0723072db371b2703578589886b
williamferreiradev <119333308+williamferreiradev@users.noreply.github.com>
2022-12-14 20:35:19 -0300

    dark mode
    Dark mode

--- js/app.js
+++ js/app.js
@@ -17,6 +17,10 @@ const nextBtn = document.querySelector(".next-btn");
 
 const links = document.querySelectorAll(".nav-link");
 
+
+const toggle_btn = document.querySelector(".toggle-btn");
+
+
 window.addEventListener("scroll", () => {
     activeLink();
     skillsCounter();
@@ -158,3 +162,32 @@ function activeLink() {
 }
 
 activeLink();
+
+
+
+
+
+
+
+// DARK MODE
+
+let firstTheme = localStorage.getItem("dark");
+
+changeTheme(+firstTheme);
+
+function changeTheme(isDark){
+    if(isDark){
+        document.body.classList.add("dark");
+        toggle_btn.classList.replace("uil-moon", "uil-sun")
+        localStorage.setItem("dark", 1);
+    }
+    else{
+        document.body.classList.remove("dark");
+        toggle_btn.classList.replace("uil-sun", "uil-moon")
+        localStorage.setItem("dark", 0);
+    }
+}
+
+toggle_btn.addEventListener("click", () => {
+    changeTheme(!document.body.classList.contains("dark"));
+})
